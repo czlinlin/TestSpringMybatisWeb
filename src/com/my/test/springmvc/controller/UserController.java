6a6d0f76ef82877e65b77d0281009f52ad0e013e
phoenix <wengliang_1986@126.com>
2017-04-30 14:44:21 +0800

    规范化配置-增加spring配置文件包

--- src/com/my/test/springmvc/controller/UserController.java
+++ src/com/my/test/springmvc/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.my.test.springmvc;
+package com.my.test.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
